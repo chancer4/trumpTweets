00ca3653f1875619bd026d1bd700d4d133a33730
chancer4 <chance.rhodes5@gmail.com>
2015-11-19 13:36:25 -0500

    trump search url

--- controller.js
+++ controller.js
@@ -1,5 +1,9 @@
 var twitterApp = angular.module('twitterApp', ['ngRoute']);
 
+var URL = “https://api.twitter.com/1.1/search/tweets.json?q=%23trump”;
+
+
 twitterApp.controller('twitterCtrl', function ($scope, $ngRoute, $http){
+	$http.get()
 
 })
