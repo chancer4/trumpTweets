3aa81e77a4fba288c3086c6d4acbab56b5202ba7
chancer4 <chance.rhodes5@gmail.com>
2015-11-19 13:54:10 -0500

    combines new tweet
    combines new tweet to old tweets and removes oldest if above max number
    of tweets

--- controller.js
+++ controller.js
@@ -1,9 +1,18 @@
 var twitterApp = angular.module('twitterApp', ['ngRoute']);
 
-var URL = “https://api.twitter.com/1.1/search/tweets.json?q=%23trump”;
+var trumpURL = "https://api.twitter.com/1.1/search/tweets.json?q=%23trump";
 
 
 twitterApp.controller('twitterCtrl', function ($scope, $ngRoute, $http){
-	$http.get()
-
-})
+	$scope.tweetList = [];
+	maxNumTweets =  100;
+	$scope.getTweets = function(){
+	$http.get(trumpURL).success(function (tweetData){
+		tweetList = tweetData.statuses.results.concat(tweetList);
+		var tweetLength = tweetList.length; 
+		if tweetLength > maxNumTweets{
+			tweetList.splice(maxNumTweets, (tweetLength-1))
+		}
+	})
+	}
+});
