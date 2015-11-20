e27f9c6c4a6cb625ae8c7560424281e70ee31779
Griffin Hammer <grifhammer@gmail.com>
2015-11-20 13:26:56 -0500

    Added Angular routing
    Changed setup to angular routing.

--- controller.js
+++ controller.js
@@ -1,8 +1,25 @@
-var twitterApp = angular.module('twitterApp', []);
-// 'ngRoute'
+var twitterApp = angular.module('twitterApp', ['ngRoute']);
 var trumpURL = "http://ec2-52-34-116-224.us-west-2.compute.amazonaws.com/trump-tweets/?hash=trump&secondHash=clinton";
 var exJson = "example.json"
-// , $ngRoute
+
+twitterApp.config(function($routeProvider){
+	$routeProvider.when('/', {
+		templateUrl: 'select-view.html',
+		controller: 'selectPersonCtrl'
+	}).
+	when('/:personName', {
+		templateUrl: 'tweet-view.html',
+		controller: 'twitterCtrl'
+	}).
+	otherwise({
+		redirectTo: '/'
+	});
+});
+
+twitterApp.controller('selectPersonCtrl', function ($scope){
+
+});
+
 twitterApp.controller('twitterCtrl', function ($scope, $http){
 	$scope.tweetList = [];
 	maxNumTweets =  100;
